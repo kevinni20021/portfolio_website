fbdd8766f4120e86491fb6a6dcf87a001b5e4732
Kevin Ni <kevin.kni.ni@gmail.com>
2022-10-27 18:16:52 -0400

    Landing Banner Complete

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Typewriter } from 'react-simple-typewriter'
+import background from './background.jpg' 
+import './landing.css'
 
 function Landing() {
   return (
-    <div>
-        
+    <div className='landing-container'>
+        <img src={background} alt="background" className='landing-background'>
+        </img>
+        <div className='landing-text'>
+        Hi, my name is Kevin and I am passionate about <br></br>
+        &nbsp;
+        <Typewriter
+            words={['Software Engineering.', 'Machine Learning.', 'Data Science.', 'Financial technology.', 'Computer Vision.']}
+            loop={true}
+            cursors
+            cursorStyle='|'
+            typeSpeed={80}
+            deleteSpeed={100}
+            delaySpeed={2000}
+          />
+        </div>
     </div>
   )
 }
